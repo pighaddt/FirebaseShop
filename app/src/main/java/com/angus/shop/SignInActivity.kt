8e6120db1363b35a5fdcfdb85adf7f3cc8311b26
angus <pighaddt@gmail.com>
2021-03-18 11:46:04 +0800

    google login setting (not Firebase UI)

--- app/src/main/java/com/angus/shop/SignInActivity.kt
+++ app/src/main/java/com/angus/shop/SignInActivity.kt
@@ -1,41 +1,89 @@
 package com.angus.shop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
+    companion object{
+        private val RC_GOOGLE_SIGN_IN = 1
+        private lateinit var googleSignInClient: GoogleSignInClient
+        private val TAG = SignInActivity::class.java.simpleName
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        //google sign in
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this@SignInActivity, gso)
+        google_sign_in.setOnClickListener {
+            startActivityForResult(googleSignInClient.signInIntent, RC_GOOGLE_SIGN_IN)
+        }
 
         //sign up onClick
         btn_signup.setOnClickListener {
-            SignUp()
+            signUp()
         }
         //login
         btn_login.setOnClickListener {
-            val email = edit_email.text.toString()
-            val password = edit_password.text.toString()
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener {  task ->
+            login()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_GOOGLE_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account = task.getResult(ApiException::class.java)
+            Log.d(TAG, "onActivityResult: ${account?.id}")
+            val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+            FirebaseAuth.getInstance()
+                .signInWithCredential(credential)
+                .addOnCompleteListener { task ->
                     if (task.isSuccessful){
                         setResult(RESULT_OK)
                         finish()
                     }else{
-                        AlertDialog.Builder(this@SignInActivity)
-                            .setTitle("Login")
-                            .setMessage(task.exception?.message)
-                            .setPositiveButton("OK", null)
-                            .show()
+                        Log.d(TAG, "onActivityResult: ${task.exception?.message}")
+                        Snackbar.make(google_sign_in, "Firebase Authentication failed", Snackbar.LENGTH_SHORT).show()
                     }
                 }
         }
     }
 
-    private fun SignUp() {
+    private fun login() {
+        val email = edit_email.text.toString()
+        val password = edit_password.text.toString()
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    setResult(RESULT_OK)
+                    finish()
+                } else {
+                    AlertDialog.Builder(this@SignInActivity)
+                        .setTitle("Login")
+                        .setMessage(task.exception?.message)
+                        .setPositiveButton("OK", null)
+                        .show()
+                }
+            }
+    }
+
+    private fun signUp() {
         val email = edit_email.text.toString()
         val password = edit_password.text.toString()
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
